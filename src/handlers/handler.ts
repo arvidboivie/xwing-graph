928420aa2c967049f4fb0a82d7b8879095782c8e
Arvid Boivie <arvid.boivie@gmail.com>
2018-09-11 21:09:01 +0200

    Add aws lambda

---
+++ src/handlers/handler.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { GraphQLServerLambda } from "graphql-yoga";
+import { ShipResolver } from "../resolvers/ship.resolver";
+import { buildSchemaSync } from "type-graphql";
+
+// build TypeGraphQL executable schema
+const schema = buildSchemaSync({
+    resolvers: [ShipResolver],
+});
+
+// Create GraphQL server
+const server = new GraphQLServerLambda({ schema });
+
+export default server.graphqlHandler;
---
+++ src/handlers/playground.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { GraphQLServerLambda } from "graphql-yoga";
+import { ShipResolver } from "../resolvers/ship.resolver";
+import { buildSchemaSync } from "type-graphql";
+import { APIGatewayEvent, Context, Callback } from "aws-lambda";
+
+// build TypeGraphQL executable schema
+const schema = buildSchemaSync({
+    resolvers: [ShipResolver],
+});
+
+// Create GraphQL server
+// const playground = (
+//     event: APIGatewayEvent,
+//     context: Context,
+//     callback: Callback
+// ) => {
+//     return new GraphQLServerLambda({ schema }).playgroundHandler(
+//         event,
+//         context,
+//         callback
+//     );
+// }
+
+const playground = new GraphQLServerLambda({ schema });
+
+export default playground.playgroundHandler;
