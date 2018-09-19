a8764d8efc568559e1d904853e6ddda74f5fc279
Arvid Boivie <arvid.boivie@gmail.com>
2018-09-19 16:45:39 +0200

    Add UpgradeResolver to lambda handlers

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -2,10 +2,11 @@ import "reflect-metadata";
 import { GraphQLServerLambda } from "graphql-yoga";
 import { ShipResolver } from "../resolvers/ship.resolver";
 import { buildSchemaSync } from "type-graphql";
+import { UpgradeResolver } from "../resolvers/upgrade.resolver";
 
 // build TypeGraphQL executable schema
 const schema = buildSchemaSync({
-  resolvers: [ShipResolver]
+  resolvers: [ShipResolver, UpgradeResolver]
 });
 
 // Create GraphQL server
--- src/handlers/playground.ts
+++ src/handlers/playground.ts
@@ -2,25 +2,12 @@ import "reflect-metadata";
 import { GraphQLServerLambda } from "graphql-yoga";
 import { ShipResolver } from "../resolvers/ship.resolver";
 import { buildSchemaSync } from "type-graphql";
-import { APIGatewayEvent, Context, Callback } from "aws-lambda";
+import { UpgradeResolver } from "../resolvers/upgrade.resolver";
 
 // build TypeGraphQL executable schema
 const schema = buildSchemaSync({
-  resolvers: [ShipResolver]
+  resolvers: [ShipResolver, UpgradeResolver]
 });
-
-// Create GraphQL server
-// const playground = (
-//     event: APIGatewayEvent,
-//     context: Context,
-//     callback: Callback
-// ) => {
-//     return new GraphQLServerLambda({ schema }).playgroundHandler(
-//         event,
-//         context,
-//         callback
-//     );
-// }
 
 const playground = new GraphQLServerLambda({ schema });
 
